078441da6e108c3a3aa47dcc29357743e94017de
Hack Reactor Students <accounts@hackreactor.com>
2015-09-25 15:40:31 -0700

    Images are now written as png files

--- Server/config/helpers.js
+++ Server/config/helpers.js
@@ -10,5 +10,19 @@ module.exports = {
     // send error message to client
     // message for gracefull error handling on app
     res.send(500, {error: error.message});
+  },
+
+  decodeBase64Image: function(dataString) {
+    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+      response = {};
+
+    if (matches.length !== 3) {
+      return new Error('Invalid input string');
+    }
+    response.type = matches[1];
+    console.log("response.type is", response.type);
+    response.data = new Buffer(matches[2], 'base64');
+
+    return response;
   }
 };
