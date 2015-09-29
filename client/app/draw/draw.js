120bd448df1ccd8181011d2eff021690d1b03016
jessicalc <jessica@crayoncollective.com>
2015-09-28 23:32:31 -0700

    re #30 added success message on successful image submission
    This is just for new users submitting images. If the user revisits the
    same URL, they'll see the same canvas and can draw over it. We'll need
    to build logic that stops users from re-drawing!

--- client/app/draw/draw.js
+++ client/app/draw/draw.js
@@ -4,16 +4,19 @@ angular.module('spliced.draw', [])
   // drawing info will go here.
   $scope.data = {};
 
-
   $scope.data.drawing = {};
   $scope.data.playerId = $route.current.params.playerId;
   $scope.data.drawing.version = 0;
+  $scope.data.submitted = $scope.data.submitted || false;
 
+  $scope.data.success = "Success! Your image has been submitted!"
+  
   // $scope.data.image = // ng-model for the canvas itself, which we'll save
 
   $scope.save = function() { 
     var image = document.getElementById("pwCanvasMain").toDataURL();  
     Draw.save(image, $scope.data.playerId);
+    $scope.data.submitted = true;
     // send the image to the server.
   };
 
