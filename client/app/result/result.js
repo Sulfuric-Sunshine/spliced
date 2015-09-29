96e51a28b41fbc45b906c89611e172f6470cee83
jessicalc <jessica@crayoncollective.com>
2015-09-29 00:24:32 -0700

    fixed message

--- client/app/result/result.js
+++ client/app/result/result.js
@@ -4,14 +4,11 @@ angular.module('spliced.result', [])
 
   $scope.data = {};
 
-  $scope.data.message = $scope.data.message || '';
+  $scope.data.pleaseWaitMessage = 'Looks like your collaborators are still drawing. Check back soon for your final drawing!';
 
   $scope.getFinalImage = function() {
     Draw.getFinalImage(function(finalImageURL) {
       $scope.data.finalImageURL = finalImageURL;
-    }, function() {
-      console.log("The image isn't there");
-      $scope.data.message = "Looks like your collaborators are still drawing. Check back soon for your final drawing!";
     });
   }
 
