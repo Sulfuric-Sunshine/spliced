f55579339e397850339bedfded3ea219fa3cebd5
jessicalc <jessica@crayoncollective.com>
2015-09-28 17:24:09 -0700

    When someone goes to the game page, they see the final image
    TO DO: Build logic that shows different stuff on game page depending on
    if game is in progress

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -3,23 +3,12 @@ angular.module('spliced.services', [])
 .factory('Draw', function($http) {
   var services = {};
 
-  services.getDrawings = function() {
-    return $http({
-      method: 'GET',
-      url: '/game/',
-
-    }).then(function(response) {
-      //this is going to be a JSON object
-      // with at least a boolean value indicating if the game is done or not
-      // if(true) - there will be four objects similar to above
-      //but it will look like this:
-      // {"isFinished" : true/false, 
-      // "player1": "urlHere"
-      // "player2": "urlHere"
-      // "player3": "urlHere"
-      // "player4": "urlHere"
-      //}
-      return response.data;
+  services.getFinalImage = function(callback, errorCallback) {
+    $http.get('/game')
+    .then(function(finalImageURL) { 
+      callback(finalImageURL.data)
+    }, function(err) {
+      errorCallback();
     })
   }
 
