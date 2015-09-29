ab1291cac4f3fd1c5f82d4b8b0e2a7a3e4a0ebe6
Morgzilla <adamhedge@gmail.com>
2015-09-29 16:45:34 -0700

    Modified grunt file for deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
     },*/
     shell: {
       prodServer: {
-        command: 'git push azure master',
+        command: 'git push heroku master',
         options: {
           stdout: true,
           stderr: true,
